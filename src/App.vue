5f717703e1b89133a897385af7551bb8001d193b
Eduard <shartner2006@mail.ru>
2026-07-20 23:30:17 +0500

    Добавление скрипта Google Tag Manager для отслеживания событий

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,18 @@ useHead({
       href: () => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg',
     },
   ],
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=AW-18338223921',
+      async: true,
+    },
+    {
+      innerHTML: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-18338223921');`,
+    },
+  ],
 })
 </script>
 
